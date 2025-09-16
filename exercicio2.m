f3f0fbf682e5803ce3f2505bffd29120d1e63a19
Felps3595 <78907094+Felps3595@users.noreply.github.com>
2025-09-16 11:14:48 -0300

    tentativa 1

--- exercicio2.m
+++ exercicio2.m
@@ -1,6 +1,6 @@
 function [blendA, blendB] = exercicio2(NG)
 
-% NG: numero do grupo
+% NG: 8
 
 % nao alterar: inicio
 es = 1;
@@ -10,10 +10,21 @@ pkg load optim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
+
+f=[10,14]';
+
+x=[1,1]';
+
+A=[-0.3,-0.2;
+  -0.2,-0.25;
+  -0.25,-0.3;
+  1,0;
+  0,1];
+b=[-15;-10;-12;20;50];
+
+x=linprog(f,A,b)
 
 
-% mantenha essas duas linhas finais
 blendA = x(1);
 blendB = x(2);
 
